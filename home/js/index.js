a079fae37e997c21aa20b9b45f46f30896f64bd0
Neemias Renan <69238611+Neemias-Renan@users.noreply.github.com>
2021-08-02 03:08:44 -0300

    Adições

---
+++ home/js/index.js
@@ -0,0 +1,20 @@
+function login(){
+    x = document.getElementById("login")
+    x.style.display = "flex"
+}
+
+function backLogin(){
+    x = document.getElementById("login")
+    x.style.display = "none"
+}
+
+function register(){
+    x = document.getElementById("register")
+    x.style.display = "flex"
+}
+
+function backRegister(){
+    x = document.getElementById("register")
+    x.style.display = "none"
+}
+
---
+++ home/js/principal.js
@@ -0,0 +1,21 @@
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    draggable: true,
+    dots: '.dots',
+    // arrows: {
+    //   prev: '.glider-prev',
+    //   next: '.glider-next'
+    // }
+  });
+
+  new Glider(document.querySelector('.glider2'), {
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    draggable: true,
+    dots: '.dots',
+    // arrows: {
+    //   prev: '.glider-prev',
+    //   next: '.glider-next'
+    // }
+  });
